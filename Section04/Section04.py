ea710970cd6eafab12643f7946a6390fa8066242
{yuto} <okimura20000621@gmail.com>
2024-07-02 13:19:15 +0900

    ログ作成プログラムを追加

---
+++ Section04/Section04.py
@@ -0,0 +1,11 @@
+import datetime
+
+#現在時刻取得
+dateNow = datetime.datetime.now()
+
+#ファイル名定義
+fileName = dateNow.strftime('%Y%m%d_%H%M%S%f')[:-3] + '.log' 
+
+#実行ログ記録
+logFile = open('.\log\%s' %(fileName), 'w')
+logFile.write('[%s]\t%s' %(dateNow.strftime('%Y-%m-%d_%H:%M:%S.%f')[:-3], 'execute!'))
